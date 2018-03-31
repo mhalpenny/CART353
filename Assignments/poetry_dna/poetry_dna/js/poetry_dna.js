5470170fb562a782a29a09e5a6c32568f26769de
mhalpenny <Matthew@Hosaka.local>
2018-03-31 14:40:24 -0400

    Adjectives

--- Assignments/poetry_dna/poetry_dna/js/poetry_dna.js
+++ Assignments/poetry_dna/poetry_dna/js/poetry_dna.js
@@ -1,6 +1,9 @@
 var s;
 //create an empty string to translate array
 var sTemp = '';
+var geneLength;
+//create an output RiString
+var output = '';
 
 //before loading the page execute this function...
 function preload() {
@@ -35,6 +38,7 @@ function setup() {
   //process through rita
   processRita();
 
+
 }
 
 function processRita() {
@@ -44,11 +48,10 @@ function processRita() {
   //analyze linguistics of the string
   var words = rs.words();
   var pos = rs.pos();
+  var add = false;
   // console.log(words);
   // console.log(pos);
 
-  //create an output RiString
-  var output = '';
   //variable for noun count
   var nouns = 0;
   //calculate the number of nouns in the string
@@ -63,13 +66,25 @@ function processRita() {
   for (var i = 0; i < words.length; i++) {
     //create a mutation number so only a fixed number can mutate
     var mutate = random(nouns);
+    var addition = random(nouns);
     //if the part of speech is a noun, randomize it
-    if ((pos[i] === 'nn') && (mutate < (nouns)/6)) {
+    if ((pos[i] === 'nn') && (addition < (nouns)/6)) {
+      //output a random adjective
+      output += lexicon.randomWord('jj');
+      //add a space
+      output += ' ';
+      //add a words from the original string back into the output
+      output += words[i];
+      //add a space
+      output += ' ';
+      //tell Rita a word has been added
+      add = true;
+    } else if ((pos[i] === 'nn') && (mutate < (nouns)/3) && add == false) {
       //output a random noun
       output += lexicon.randomWord('nn');
       //add a space
       output += ' ';
-    } else {
+  } else {
       //add a words from the original string back into the output
       output += words[i];
       //add a space
@@ -80,12 +95,18 @@ function processRita() {
 
   text(output, width / 2, height / 2);
 
-$.ajax({
-type: "POST",
-url: "saveToFile.php",
- data : {'stringData':output},
-success: function(resultData){
-          alert("Save Complete");
-      }
-});
+saveText();
+
 }
+
+function saveText(){
+
+  $.ajax({
+  type: "POST",
+  url: "saveToFile.php",
+   data : {'stringData':output},
+  });
+
+}
+//function crossOver();
+//function crossOverPoem();
